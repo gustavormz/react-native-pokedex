0431a6a1e77a3fd7877e51c8d1fcb927a5b7340b
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-05-06 21:26:52 -0600

    navigator config for splashscreen

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import {
+	NavigationContainer
+} from '@react-navigation/native';
+import {
+	createStackNavigator
+} from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SplashscreenView from './views/splashscreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => (
+	<NavigationContainer>
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false
+			}}>
+			<Stack.Screen
+				name={"Splashscreen"}
+				component={SplashscreenView}/>
+		</Stack.Navigator>
+	</NavigationContainer>
+);
+
+export default App;
