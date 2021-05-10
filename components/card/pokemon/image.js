d12c6e4e40664f7047c36dbdee701760b38b8f42
DiegoGustavoRamirez <diegormz111@gmail.com>
2021-05-09 20:25:26 -0600

    added card pokemon image as stateless component to display pokemon image

---
+++ components/card/pokemon/image.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    StyleSheet
+} from 'react-native';
+import {
+    Avatar,
+    Surface
+} from 'react-native-paper';
+import PropTypes from 'prop-types';
+
+const CardPokemonImage = ({
+    imageUri,
+    avatarSize
+}) => (
+    <Surface style={styles.container}>
+        <Avatar.Image
+            style={styles.avatar}
+            size={avatarSize}
+            source={{
+                uri: imageUri
+            }}/>
+    </Surface>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 40,
+        paddingHorizontal: 20,
+        display: `flex`,
+        alignItems: `center`,
+        elevation: 3,
+        borderRadius: 5,
+        backgroundColor: `green`
+    },
+    avatar: {
+        backgroundColor: `white`
+    }
+});
+
+CardPokemonImage.propTypes = {
+    avatarSize: PropTypes.number,
+    imageUri: PropTypes.string
+};
+
+CardPokemonImage.defaultProps = {
+    avatarSize: 150
+};
+
+export default CardPokemonImage;
